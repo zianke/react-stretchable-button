2f42725ec40cefd248fcc1beebdcc48fb484912d
Zian Ke <kezian@umich.edu>
2018-03-01 09:40:09 -0500

    fix position of icon and text

--- src/App.js
+++ src/App.js
@@ -5,29 +5,32 @@ import './StretchableButton'
 import StretchableButton from "./StretchableButton";
 
 class App extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {stretchableButtonZDepth: 2};
-    }
+  constructor(props) {
+    super(props);
+    this.state = { stretchableButtonZDepth: 2 };
+  }
 
-    render() {
-        return (
-            <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo"/>
-                    <h1 className="App-title">Welcome to React</h1>
-                </header>
-                <p className="App-intro">
-                    To get started, edit <code>src/App.js</code> and save to reload.
-                </p>
-                <StretchableButton zDepth={this.state.stretchableButtonZDepth} onMouseEnter={() => {
-                    this.setState({stretchableButtonZDepth: 3});
-                }} onMouseLeave={() => {
-                    this.setState({stretchableButtonZDepth: 2});
-                }}/>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+        <StretchableButton height={50} width={[50, 100]} circle align="left" style={{
+          width: 100,
+          backgroundColor: 'rgb(0,0,255)'
+        }} zDepth={this.state.stretchableButtonZDepth} onMouseEnter={() => {
+          this.setState({ stretchableButtonZDepth: 3 });
+        }} onMouseLeave={() => {
+          this.setState({ stretchableButtonZDepth: 2 });
+        }} />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/StretchableButton.js
+++ src/StretchableButton.js
@@ -2,38 +2,72 @@ import React, {Component} from 'react';
 
 
 const zDepth = [{
-    boxShadow: '0 0 0 rgba(0, 0, 0, 0), 0 0 0 rgba(0, 0, 0, 0)'
+  boxShadow: '0 0 0 rgba(0, 0, 0, 0), 0 0 0 rgba(0, 0, 0, 0)'
 }, {
-    boxShadow: '0 1px 6px rgba(0,0,0,0.12), 0 1px 4px rgba(0,0,0,0.12)'
+  boxShadow: '0 1px 6px rgba(0,0,0,0.12), 0 1px 4px rgba(0,0,0,0.12)'
 }, {
-    boxShadow: '0 3px 10px rgba(0,0,0,0.16), 0 3px 10px rgba(0,0,0,0.23)'
+  boxShadow: '0 3px 10px rgba(0,0,0,0.16), 0 3px 10px rgba(0,0,0,0.23)'
 }, {
-    boxShadow: '0 10px 30px rgba(0,0,0,0.19), 0 6px 10px rgba(0,0,0,0.23)'
+  boxShadow: '0 10px 30px rgba(0,0,0,0.19), 0 6px 10px rgba(0,0,0,0.23)'
 }, {
-    boxShadow: '0 14px 45px rgba(0,0,0,0.25), 0 10px 18px rgba(0,0,0,0.22)'
+  boxShadow: '0 14px 45px rgba(0,0,0,0.25), 0 10px 18px rgba(0,0,0,0.22)'
 }, {
-    boxShadow: '0 19px 60px rgba(0,0,0,0.3), 0 15px 20px rgba(0,0,0,0.22)'
+  boxShadow: '0 19px 60px rgba(0,0,0,0.3), 0 15px 20px rgba(0,0,0,0.22)'
 }];
 
 const buttonSytle = {
-    width: 100,
-    backgroundColor: 'red',
-    transition: '.25s all',
+  transition: '.25s all'
 };
 
 class StretchableButton extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {hovered: false};
-    }
+  constructor(props) {
+    super(props);
+    this.state = { hovered: false };
+    this.handleMouseEnter = this.handleMouseEnter.bind(this);
+    this.handleMouseLeave = this.handleMouseLeave.bind(this);
+  }
 
-    render() {
-        return (
-            <div {...this.props} style={Object.assign({}, buttonSytle, zDepth[this.props.zDepth])}>
-                test
-            </div>
-        );
-    }
+  handleMouseEnter() {
+    this.setState({ hovered: true });
+  }
+
+  handleMouseLeave() {
+    this.setState({ hovered: false });
+  }
+
+  render() {
+    return (
+      <div
+        style={{
+          width: this.props.width[1],
+          height: this.props.height,
+          backgroundColor: 'red',
+          position: 'relative',
+        }}>
+        <div {...this.props}
+             style={Object.assign({}, buttonSytle, this.props.style, {
+               height: this.props.height,
+               width: this.state.hovered ? this.props.width[1] : this.props.width[0],
+               borderRadius: this.props.circle && this.props.height && this.props.height / 2,
+               float: this.props.align === 'right' && 'right'
+             }, zDepth[this.props.zDepth])}
+             onMouseEnter={this.handleMouseEnter} onMouseLeave={this.handleMouseLeave}>
+        </div>
+        <div style={Object.assign({
+          position: 'absolute',
+          top: 0
+        }, this.props.align === 'right' ? { right: 0 } : { left: 0 })}>
+          icon
+        </div>
+        <div style={Object.assign({
+          position: 'absolute',
+          top: 0
+        }, this.props.align === 'right' ? { left: 0 } : { right: 0 })}>
+          text
+        </div>
+      </div>
+    );
+  }
 }
 
 export default StretchableButton;
