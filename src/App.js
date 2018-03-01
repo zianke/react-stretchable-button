b3472f9cccfd583883cbfbed4fc188748f71dd91
Zian Ke <kezian@umich.edu>
2018-03-01 11:11:46 -0500

    fix onMouseLeave bug

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
+      <div className="App" style={{ /*{backgroundColor:'rgb(100,100,100)'}*/ }}>
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
@@ -20,14 +20,22 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-        <StretchableButton height={50} width={[50, 100]} circle align="left" style={{
-          width: 100,
-          backgroundColor: 'rgb(0,0,255)'
-        }} zDepth={this.state.stretchableButtonZDepth} onMouseEnter={() => {
+        <StretchableButton
+          height={50}
+          width={[50, 100]}
+          stretchPadding={25}
+          circle
+          align="right"
+          style={{
+            width: 100,
+            backgroundColor: 'rgb(0,0,255)',
+          }} zDepth={this.state.stretchableButtonZDepth} onMouseEnter={() => {
           this.setState({ stretchableButtonZDepth: 3 });
         }} onMouseLeave={() => {
           this.setState({ stretchableButtonZDepth: 2 });
-        }} />
+        }}>
+          <span>A</span>
+        </StretchableButton>
       </div>
     );
   }
--- src/StretchableButton.js
+++ src/StretchableButton.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import logo from './logo.svg';
 
 
 const zDepth = [{
@@ -19,6 +20,28 @@ const buttonSytle = {
   transition: '.25s all'
 };
 
+const buttonLayerStyle = {
+  transition: '.25s all',
+  opacity: 0
+}
+
+const emergeStyle = {
+  transition: '.25s all',
+  /*
+    transitionDelay: '.25s'
+  */
+};
+
+const centerParentStyle = {
+  display: 'table'
+};
+
+const centerChildStyle = {
+  display: 'table-cell',
+  verticalAlign: 'middle',
+  textAlign: 'center',
+};
+
 class StretchableButton extends Component {
   constructor(props) {
     super(props);
@@ -27,11 +50,11 @@ class StretchableButton extends Component {
     this.handleMouseLeave = this.handleMouseLeave.bind(this);
   }
 
-  handleMouseEnter() {
+  handleMouseEnter(e) {
     this.setState({ hovered: true });
   }
 
-  handleMouseLeave() {
+  handleMouseLeave(e) {
     this.setState({ hovered: false });
   }
 
@@ -41,29 +64,52 @@ class StretchableButton extends Component {
         style={{
           width: this.props.width[1],
           height: this.props.height,
-          backgroundColor: 'red',
           position: 'relative',
         }}>
+        <div
+          style={Object.assign({}, buttonSytle, this.props.style, {
+            height: this.props.height,
+            width: this.state.hovered ? this.props.width[1] : this.props.width[0],
+            borderRadius: this.props.circle && this.props.height && this.props.height / 2,
+            float: this.props.align === 'right' && 'right'
+          }, zDepth[this.props.zDepth])}
+        >
+          <div style={Object.assign({
+            height: this.props.height,
+            width: this.props.width[0],
+            float: this.props.align === 'right' && 'right'
+          }, centerParentStyle)}>
+            <div style={centerChildStyle}>
+              {this.props.children}
+            </div>
+          </div>
+        </div>
+        <div style={Object.assign({
+          position: 'absolute',
+          top: 0,
+          height: this.props.height,
+          opacity: this.state.hovered ? 1 : 0
+        }, centerParentStyle, emergeStyle, this.props.align === 'right' ? { left: this.props.stretchPadding } : { right: this.props.stretchPadding })}>
+          <div style={centerChildStyle}>
+            <span>BBB</span>
+          </div>
+        </div>
         <div {...this.props}
-             style={Object.assign({}, buttonSytle, this.props.style, {
+             style={Object.assign({}, { cursor: this.props.style.cursor ? this.props.style.cursor : 'pointer' }, {
+               position: 'absolute',
+               top: 0,
                height: this.props.height,
                width: this.state.hovered ? this.props.width[1] : this.props.width[0],
                borderRadius: this.props.circle && this.props.height && this.props.height / 2,
-               float: this.props.align === 'right' && 'right'
-             }, zDepth[this.props.zDepth])}
-             onMouseEnter={this.handleMouseEnter} onMouseLeave={this.handleMouseLeave}>
-        </div>
-        <div style={Object.assign({
-          position: 'absolute',
-          top: 0
-        }, this.props.align === 'right' ? { right: 0 } : { left: 0 })}>
-          icon
-        </div>
-        <div style={Object.assign({
-          position: 'absolute',
-          top: 0
-        }, this.props.align === 'right' ? { left: 0 } : { right: 0 })}>
-          text
+             }, buttonLayerStyle, this.props.align === 'right' ? { right: 0 } : { left: 0 })}
+             onMouseEnter={(e) => {
+               this.props.onMouseEnter(e);
+               this.handleMouseEnter(e);
+             }}
+             onMouseLeave={(e) => {
+               this.props.onMouseLeave(e);
+               this.handleMouseLeave(e);
+             }}>
         </div>
       </div>
     );
