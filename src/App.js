82f7665a3067bf2cafe4fb82dad7f7540445dd5b
Zian Ke <kezian@umich.edu>
2018-03-01 18:27:29 -0500

    remove useless parts

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import React, {Component} from 'react';
-import logo from './logo.svg';
-import './App.css';
 import './StretchableButton'
 import StretchableButton from "./StretchableButton";
 
@@ -15,14 +13,7 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App" style={{ /*{backgroundColor:'rgb(100,100,100)'}*/ }}>
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div style={{ fontFamily: 'sans-serif' }}>
         <StretchableButton
           style={{ position: 'absolute', right: 0, bottom: 0, margin: 20 }}
           alignRight
