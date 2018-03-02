5c54ab7696a32b68434fd55eb13ae47746f8739b
Zian Ke <kezian@umich.edu>
2018-03-02 00:43:51 -0500

    fix import bug in demo.js

--- src/demo.js
+++ src/demo.js
@@ -1,5 +1,4 @@
 import React, {Component} from 'react';
-import './StretchableButton'
 import StretchableButton from "./react-stretchable-button";
 
 import FontAwesomeIcon from '@fortawesome/react-fontawesome'
